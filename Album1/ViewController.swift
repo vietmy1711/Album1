c209671f21972387d208e5cd0aee231422eac5ba
Nguyen Viet My <zzvietmyzz@gmail.com>
2020-07-13 13:41:02 +0700

    Initial Commit

---
+++ Album1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Album1
+//
+//  Created by MM on 7/13/20.
+//  Copyright © 2020 MM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
